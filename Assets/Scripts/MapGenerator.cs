5bbb673dd7acd2db619899342471be2d3446fd65
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-05-17 19:42:08 +0300

    Created basic map generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+    [Tooltip("Tilemap used for terrain tiles")] [SerializeField]
+    Tilemap tilemap;
+
+    [Tooltip("Tile used to fill the play area")] [SerializeField]
+    Tile tile;
+
+    [Tooltip("Width of the map in tiles")] [SerializeField]
+    private int Width;
+
+    [Tooltip("Height of the map in tiles")] [SerializeField]
+    private int Height;
+
+    [Tooltip("Map offset, applied for all tiles")] [SerializeField]
+    private Vector3Int Offset;
+
+    void Generate(int width, int height, Vector3Int offset)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector3Int position = new Vector3Int(i, j, 0) + offset;
+                tilemap.SetTile(position, tile);
+            }
+        }
+    }
+
+    public void Start()
+    {
+        Generate(Width, Height, Offset);
+    }
+}
