f3c792b886a8c3840f0b52d1d0ed7096a4f738f4
temelehtone <91484193+temelehtone@users.noreply.github.com>
2022-05-18 11:42:33 +0300

    Removed unnecessary AStar files and made own pathfinding script

---
+++ Assets/Scripts/AIPathfinding.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AIPathfinding : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] Transform enemyGraph;
+
+    [SerializeField] float speed = 200f;
+    [SerializeField] float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null) return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        } 
+        Vector2 direction = ((Vector2) path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (force.x >= 0.01f)
+        {
+            enemyGraph.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (force.y >= 0.01f)
+        {
+            enemyGraph.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+}
