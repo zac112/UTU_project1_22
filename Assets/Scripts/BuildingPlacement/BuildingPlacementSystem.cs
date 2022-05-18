778bf52d152d55ec9f2a8b6a95728934f52e8e41
Daniel Bergman <dlberg@utu.fi>
2022-05-18 21:27:31 +0300

    Added ability to select which building to place

---
+++ Assets/Scripts/BuildingPlacement/BuildingPlacementSystem.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BuildingPlacementSystem : MonoBehaviour
+{
+    [SerializeField] Transform buildingToPlace;
+    [SerializeField] List<Transform> buildableBuildings;
+
+    Tilemap tilemap;
+    Transform selectedBuilding;
+
+    void Start()
+    {
+        tilemap = GameObject.Find("Grid").GetComponentInChildren<Tilemap>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            selectedBuilding = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.K)) 
+        {
+            selectedBuilding = buildableBuildings[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            selectedBuilding = buildableBuildings[1];
+        }
+
+        if (Input.GetMouseButtonDown(1) && selectedBuilding != null) 
+        {
+            Vector3 tileLocationInWorld = GetTileOnMouse();
+
+            // Instantiate building on tileLocation
+            Instantiate(selectedBuilding, tileLocationInWorld, Quaternion.identity);
+        }
+    }
+
+    Vector3 GetTileOnMouse()
+    {
+        // Get the mouse position and store it in a variable
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Get the location of the tile under the mouse
+        Vector3Int tileLocation = tilemap.WorldToCell(mousePosition);
+
+        // Get the center of the tile under the mouse?
+        // Perhaps a bit redundant to first get the tile location in the tilemap and then turn it back to world position?
+        Vector3 tileLocationInWorld = tilemap.GetCellCenterWorld(tileLocation);
+
+        // Set Z to 0, to avoid building being underground
+        tileLocationInWorld.z = 0;
+
+        return tileLocationInWorld;
+    }
+}
