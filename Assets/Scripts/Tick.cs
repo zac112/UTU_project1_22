fab4a3b27c3c5a833910433694fd7c655d7ef087
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-05-24 10:41:38 +0300

    Created Tick.cs
    This script contains the tick delegate for the tick system.

---
+++ Assets/Scripts/Tick.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tick : MonoBehaviour
+{
+    public delegate int TickDelegate(int tickNumber);
+
+    public static TickDelegate OnTick;
+
+    // Tick values
+    private static float tickTimer;
+    private static float tickTimerMax;
+    private static int currentTick = 0;
+
+    [Tooltip("Number of game ticks per second")] [SerializeField]
+    private int tickSpeed;
+
+    private void Start()
+    {
+        tickTimerMax = 1f / tickSpeed;
+    }
+
+    private void Update()
+    {
+        tickTimer += Time.deltaTime;
+        // Perform multiple ticks if a lag spike occurs
+        while (tickTimer >= tickTimerMax)
+        {
+            tickTimer -= tickTimerMax;
+            currentTick++;
+            OnTick?.Invoke(currentTick);
+        }
+    }
+}
