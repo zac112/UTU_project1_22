4ce236c94ad0fb637039b31833ce9c95277b2ff2
Erno Lokkila <eolokk@utu.fi>
2022-05-26 20:45:38 +0300

    Prototype GUI. Add GUI prefab as child to main camera

---
+++ Assets/Scripts/GUI/GUI_Button.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Button : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject context;
+
+    [SerializeField]
+    GameObject GUI_GO;
+
+    void OnMouseDown(){        
+        Destroy(GUI_GO.transform.GetChild(0).gameObject);
+        GameObject go = Instantiate(context,Vector3.zero,Quaternion.identity);        
+        go.transform.SetParent(GUI_GO.transform);
+        go.transform.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/GUI/GUI_Init.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localPosition = new Vector3(0,-5f,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
