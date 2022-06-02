c55c98e1d2cd9081bad83fd909f3ce16d03e6673
Erno Lokkila <eolokk@utu.fi>
2022-06-02 13:11:11 +0300

    Cleaned up code: combined duplicate code to Building.cs

--- Assets/Scripts/BuildingScripts/Building.cs
+++ Assets/Scripts/BuildingScripts/Building.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmithyScript : MonoBehaviour, IBuildable
+public class Building : MonoBehaviour, IBuildable
 {
     [SerializeField] private int _width;
     public int Width
