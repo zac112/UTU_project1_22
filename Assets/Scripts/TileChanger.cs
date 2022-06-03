c254fddce00f9533dbc45292a7da68b95f8fa82d
JereLem <56977360+JereLem@users.noreply.github.com>
2022-06-03 17:15:24 +0300

    TileChanger  fix

--- Assets/Scripts/TileChanger.cs
+++ Assets/Scripts/TileChanger.cs
@@ -5,8 +5,6 @@ using UnityEngine.Tilemaps;
 
 public class TileChanger : MonoBehaviour
 {
-    public bool isActive = false;
-
     public Tilemap tilemap;
 
     public Tile tile;
@@ -15,16 +13,17 @@ public class TileChanger : MonoBehaviour
 
     public BuildingPlacementSystem bps;
 
- 
-    void Update()
+    void start()
     {
-        // Get tilemap and buildinplacementsystem
         tilemap = GameObject.Find("Grid(Clone)").GetComponentInChildren<Tilemap>();
         bps = GameObject.Find("BuildingPlacementSystem").GetComponent<BuildingPlacementSystem>();
+    }
 
+    void Update()
+    {
         // If mouse clicked change tile to given tile
         // HAS TO BE FIXED (can now change water to any tile)
-        if (isActive && Input.GetMouseButton(0)){
+        if (Input.GetMouseButton(0)){
             location = bps.GetTileCellLocation();
             location.z = 0;
             location.x = location.x -5;
