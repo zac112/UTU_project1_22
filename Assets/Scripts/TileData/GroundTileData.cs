c35988fb070ce17ec8b050ffd01a5ca96ad556d5
Daniel Bergman <dlberg@utu.fi>
2022-06-08 21:19:41 +0300

    Updated GroundTileData

--- Assets/Scripts/TileData/GroundTileData.cs
+++ Assets/Scripts/TileData/GroundTileData.cs
@@ -1,25 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class GroundTileData : MonoBehaviour
 {
-    Tilemap tilemap;
-    Grid grid;
-
-    private void Start()
-    {
-        grid = GameObject.FindGameObjectWithTag("Tilemap").GetComponent<Grid>();
-        tilemap = grid.GetComponentInChildren<Tilemap>();
-    }
-
-    public GroundTile GetGroundTileOnMouseClick() 
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int location = tilemap.WorldToCell(mousePosition);
-        location.z = 0;
-
-        return tilemap.GetTile<GroundTile>(location);
-    }
+    public bool isOccupied;
+    public bool isWalkable;
 }
