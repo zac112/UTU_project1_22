8831db8f74735f16810373fb85b43c154635b275
Daniel Bergman <dlberg@utu.fi>
2022-06-08 09:00:09 +0300

    Added IsWalkable attribute and added summaries

--- Assets/Scripts/ScriptableObjects/GroundTile.cs
+++ Assets/Scripts/ScriptableObjects/GroundTile.cs
@@ -6,6 +6,18 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(fileName = "GroundTile", menuName = "ScriptableObjects/GroundTile", order = 0)]
 public class GroundTile : Tile
 {
+    /// <summary>
+    /// Determines if a building can be built on this tile
+    /// </summary>
     public bool isOccupied = false;
-    public float moveSpeed = 1;
+
+    /// <summary>
+    /// Determines if a tile is walkable
+    /// </summary>
+    public bool isWalkable = true;
+
+    /// <summary>
+    /// Determines the movement speed multiplier of the tile
+    /// </summary>
+    public float moveSpeedMultiplier = 1;
 }
