19b5bf49ac31a80aa433833e82dcbf4f67225e2b
Daniel Bergman <dlberg@utu.fi>
2022-06-08 15:18:20 +0300

    Configured layers for each tile

--- Assets/Scripts/ScriptableObjects/GroundTile.cs
+++ Assets/Scripts/ScriptableObjects/GroundTile.cs
@@ -20,4 +20,26 @@ public class GroundTile : Tile
     /// Determines the movement speed multiplier of the tile
     /// </summary>
     public float moveSpeedMultiplier = 1;
+
+    public LayerMask layer;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+        tileData.gameObject.layer = ToLayer(layer);
+    }
+
+    /// Source: https://forum.unity.com/threads/get-the-layernumber-from-a-layermask.114553/
+    /// <summary> Converts given bitmask to layer number </summary>
+    /// <returns> layer number </returns>
+    public static int ToLayer(int bitmask)
+    {
+        int result = bitmask > 0 ? 0 : 31;
+        while (bitmask > 1)
+        {
+            bitmask = bitmask >> 1;
+            result++;
+        }
+        return result;
+    }
 }
