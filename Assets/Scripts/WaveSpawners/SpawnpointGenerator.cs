910d0536687e227ba6adcc68ba58b049913f43b9
Daniel Bergman <dlberg@utu.fi>
2022-06-12 14:24:51 +0300

    Added spawn point generator script

---
+++ Assets/Scripts/WaveSpawners/SpawnpointGenerator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnpointGenerator : MonoBehaviour
+{
+    [SerializeField] int amount = 1;
+
+    
+}
